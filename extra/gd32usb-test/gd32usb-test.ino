917f14b12dc42c85d6b730bca87f543211ae9806
Brian Cully <bjc@kublai.com>
2021-09-20 11:46:02 -0400

    WIP: we have blinky + PluggableUSB

--- extra/gd32usb-test/gd32usb-test.ino
+++ extra/gd32usb-test/gd32usb-test.ino
@@ -1,20 +1,22 @@
 #include "TestPlug.h"
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("-- Blink demo --");
+void setup()
+{
+				Serial.begin(115200);
+				Serial.println("-- Blink demo --");
 
-  pinMode(LED2, OUTPUT);
+				pinMode(LED2, OUTPUT);
 
-  Serial.println(tp.foo());
+				Serial.println(tp.foo());
 }
 
-void loop() {
-  Serial.println("On");
-  digitalWrite(LED2, HIGH);
-  delay(500);
+void loop()
+{
+				Serial.println("On");
+				digitalWrite(LED2, HIGH);
+				delay(500);
 
-  Serial.println("Off");
-  digitalWrite(LED2, LOW);
-  delay(500);
+				Serial.println("Off");
+				digitalWrite(LED2, LOW);
+				delay(500);
 }
