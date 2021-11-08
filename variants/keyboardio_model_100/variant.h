dfb650fc0abdb5c6124997d43edb6c2b417187a3
Jesse Vincent <jesse@keyboard.io>
2021-11-08 12:55:59 -0800

    Add USB config for the model 100

--- variants/keyboardio_model_100/variant.h
+++ variants/keyboardio_model_100/variant.h
@@ -123,6 +123,12 @@ extern "C" {
 #define PIN_WIRE_SDA            PB7
 #define PIN_WIRE_SCL            PB6
 
+
+#define USB_PULLUP                GPIOA
+#define USB_PULLUP_PIN            GPIO_PIN_8
+#define RCC_AHBPeriph_GPIO_PULLUP RCU_GPIOA
+
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
