5f38bd5d8d17e75580aa95e6896fea698e64565b
Brian Cully <bjc@kublai.com>
2021-11-12 16:10:46 -0500

    Whitespace and dead code removal.

--- cores/arduino/gd32/usb.h
+++ cores/arduino/gd32/usb.h
@@ -8,9 +8,5 @@ extern usb_dev usbd;
 
 void usb_init(usb_desc*, usb_class*);
 void usb_connect();
-uint8_t* usb_ep_rx_addr(uint8_t);
-uint32_t* usb_ep_rx_count(uint8_t);
-uint8_t* usb_ep_tx_addr(uint8_t);
-uint32_t* usb_ep_tx_count(uint8_t);
 
 #endif
