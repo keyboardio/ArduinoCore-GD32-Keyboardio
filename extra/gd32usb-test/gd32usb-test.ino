8a523a36af921e2790f801235c8ba7edf9d5f473
Brian Cully <bjc@kublai.com>
2021-11-12 16:26:28 -0500

    Whitespace

--- extra/gd32usb-test/gd32usb-test.ino
+++ extra/gd32usb-test/gd32usb-test.ino
@@ -2,36 +2,36 @@
 
 void setup()
 {
-				//Serial.begin(115200);
-				Serial.println("-- Blink demo --");
+    //Serial.begin(115200);
+    Serial.println("-- Blink demo --");
 
-				pinMode(LED2, OUTPUT);
+    pinMode(LED2, OUTPUT);
 
-				BootKeyboard.begin();
-				Mouse.begin();
+    BootKeyboard.begin();
+    Mouse.begin();
 }
 
 int f = 0;
 
 void loop()
 {
-				Serial.println("On");
-				digitalWrite(LED2, HIGH);
-				delay(500);
+    Serial.println("On");
+    digitalWrite(LED2, HIGH);
+    delay(500);
 
-				BootKeyboard.press(HID_KEYBOARD_A_AND_A);
-				BootKeyboard.sendReport();
-				BootKeyboard.releaseAll();
-				BootKeyboard.sendReport();
+    BootKeyboard.press(HID_KEYBOARD_A_AND_A);
+    BootKeyboard.sendReport();
+    BootKeyboard.releaseAll();
+    BootKeyboard.sendReport();
 
-				// Delay long enough to see the mouse move.
-				delay(500);
+    // Delay long enough to see the mouse move.
+    delay(500);
 
-				Mouse.move(10, 0);
-				Mouse.sendReport();
-				Mouse.releaseAll();
-				Mouse.sendReport();
+    Mouse.move(10, 0);
+    Mouse.sendReport();
+    Mouse.releaseAll();
+    Mouse.sendReport();
 
-				Serial.println("Off");
-				digitalWrite(LED2, LOW);
+    Serial.println("Off");
+    digitalWrite(LED2, LOW);
 }
