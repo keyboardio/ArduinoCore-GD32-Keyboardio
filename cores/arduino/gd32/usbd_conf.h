8592c4065b76fddadc2c7dbdc68a5ff80f3421da
Brian Cully <bjc@kublai.com>
2021-11-24 14:49:32 -0500

    Remove out-of-date comments, fix undef warning for usbd_isr.

--- cores/arduino/gd32/usbd_conf.h
+++ cores/arduino/gd32/usbd_conf.h
@@ -1,44 +1,7 @@
-/*!
-    \file    usbd_conf.h
-    \brief   usb device driver basic configuration
-
-    \version 2021-03-23, V2.0.0, demo for GD32F30x
-*/
-
-/*
-    Copyright (c) 2021, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice,
-       this list of conditions and the following disclaimer in the documentation
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors
-       may be used to endorse or promote products derived from this software without
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-OF SUCH DAMAGE.
-*/
-
 #ifndef __USBD_CONF_H
 #define __USBD_CONF_H
 
 #include "gd32f30x.h"
-
-// USB_PULLUP stuff.
-// TODO: think about how to file this away and include it.
 #include "variant.h"
 
 /*
@@ -49,10 +12,10 @@ OF SUCH DAMAGE.
 #undef USBD_LOWPWR_MODE_ENABLE
 
 /* USB feature -- Self Powered */
-/* #define USBD_SELF_POWERED */
+#undef USBD_SELF_POWERED
 
 /* link power mode support */
-/* #define LPM_ENABLED */
+#undef LPM_ENABLED
 
 /*
  * TODO: I’m currently using the maximum values allowed by the spec
